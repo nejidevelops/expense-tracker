a5155df80aecfb786945c8e72823b84e418d218d
Newton Ombese <newtonombese1@gmail.com>
2024-02-09 10:14:51 +0300

    Importing header component into App

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      Hello
+      <Header />
     </div>
   );
 }
