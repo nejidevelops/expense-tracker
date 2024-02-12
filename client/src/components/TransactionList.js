fde7e95504c037196679a92535cb0db9e1e42236
Newton Ombese <newtonombese1@gmail.com>
2024-02-12 07:19:50 +0300

    Set up Transaction List component

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const TransactionList = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <h3>History</h3>
+      <ul id="list" class="list">
+        <li class="minus">
+          Cash <span>-$400</span><button class="delete-btn">x</button>
+        </li>
+      </ul>
+    </>
   )
 }
 
