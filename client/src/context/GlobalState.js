84e6252a26f99dec75cca5e1dd52af1a5392154a
Newton Ombese <newtonombese1@gmail.com>
2024-02-12 14:37:51 +0300

    Implementing addTransaction Action

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from './AppReducer';
 
 // Initial State
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 }
-  ],
+  transactions: [],
 };
 
 // Create Context
@@ -27,8 +22,15 @@ export const Provider = ({ children }) => {
     })
   }
 
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    })
+  }
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions, deleteTransaction }}>
+    <GlobalContext.Provider value={{ transactions: state.transactions, deleteTransaction, addTransaction }}>
       {children}
     </GlobalContext.Provider>
   )
