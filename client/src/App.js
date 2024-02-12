db80125dcbef0063cf32b11a71c17d522d124799
Newton Ombese <newtonombese1@gmail.com>
2024-02-12 07:19:35 +0300

    Import TransactionList component into App

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
 
 // Styling imports
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
