551d64588bbdb7db20b3dc9ac60e8ba0be6b281e
Newton Ombese <newtonombese1@gmail.com>
2024-02-12 08:35:35 +0300

    Implementing my context API in the Transaction List component

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
+        { transactions.map(transaction => (
+          <li key={transaction.id} className='minus'>
+            {transaction.text} <span>{transaction.amount}</span><button class="delete-btn">x</button>
+          </li>
+        ))}
       </ul>
     </>
   )
