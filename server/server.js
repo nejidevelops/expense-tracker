cf4259023aaae7157113e0b3650901ab928bb2df
Newton Ombese <newtonombese1@gmail.com>
2024-02-13 16:17:39 +0300

    Successful setup of the basic server and it worked

--- server/server.js
+++ server/server.js
@@ -3,10 +3,14 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 
+dotenv.config({ path: './config/config.env' });
+
 const app = express();
 
 // Routes
 app.get('/', (req, res) => res.send('Hello World'));
 
+const PORT = process.env.PORT || 8080;
+
 // To run the server
-app.listen(5000, () => console.log('Server started on port 5000...'))
+app.listen(PORT, console.log(`Server started on port ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
