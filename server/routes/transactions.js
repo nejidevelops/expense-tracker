da7c90cd0aebfffca61a4900ea419642017f02de
Newton Ombese <newtonombese1@gmail.com>
2024-02-13 16:25:32 +0300

    Setting up transactions route

---
+++ server/routes/transactions.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const { getTransactions } = require('../controllers/transactions');
+
+router.route('/').get(getTransactions);
+
+module.exports = router;
