80430feb61c19431a5ab9ec29926cf708c7b5ee1
Newton Ombese <newtonombese1@gmail.com>
2024-02-13 17:07:07 +0300

    Setting up the the add and delete transactions controller

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -4,3 +4,17 @@
 exports.getTransactions = (req, res, next) => {
   res.send('Get transactions');
 }
+
+// @desc Add transaction
+// @route POST /api/v1/transactions
+// @access Public
+exports.addTransactions = (req, res, next) => {
+  res.send('New transactions');
+}
+
+// @desc Delete transaction
+// @route DELETE /api/v1/transactions/:id
+// @access Public
+exports.deleteTransactions = (req, res, next) => {
+  res.send('DELETE transactions');
+}
